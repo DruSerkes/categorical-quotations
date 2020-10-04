32cc91ed789a346c736ea9272939806e390642e6
Dru Serkes <DruSerkes@gmail.com>
2020-10-04 17:10:50 -0400

    add logic for flash close btn

--- static/app.js
+++ static/app.js
@@ -1,5 +1,11 @@
 // TODO
 
 // make the x kill the alert
-// make the alert auto disappear
 // add a button that will save a quote to localStorage
+
+const closeBtn = document.querySelector('.close');
+const flash = document.getElementById('flash');
+
+closeBtn.addEventListener('click', () => {
+	flash.remove();
+});
